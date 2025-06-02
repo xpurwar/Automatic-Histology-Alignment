bd75445f103475033087bfc68ed3dba5c7b45915
Anna <purwar.ananya4@gmail.com>
2025-06-02 16:45:46 -0500

    reults from sample data

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from DeepSlice import DSModel
 species = 'mouse' #available species are 'mouse' and 'rat'
 Model = DSModel(species)
 #this cell should take about 1 second per image
-folderpath = 'examples/example_brain/GLTa/'
+folderpath = '/Users/dineshupadhyay/Transluscence/Automatic-Histology-Alignment-1/ExampleBrain'
 Model.predict(folderpath, ensemble=True, section_numbers=True)
+Model.propagate_angles()
+Model.enforce_index_spacing()
+Model.save_predictions(folderpath + 'MyResults')
